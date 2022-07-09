ba3cd0ff2895e92f77211088f30b77d7935c9268
danleks <danilevich.lks@gmail.com>
2022-07-09 18:14:32 +0200

    add config

--- part4/bloglist/backend/index.js
+++ part4/bloglist/backend/index.js
@@ -1,4 +1,5 @@
 const http = require('http')
+const config = require('./utils/config')
 const logger = require('./utils/logger')
 const express = require('express')
 const app = express()
@@ -38,7 +39,6 @@ app.post('/api/blogs', (request, response) => {
         })
 })
 
-const PORT = 3003
-app.listen(PORT, () => {
-    logger.info(`Server running on port ${PORT}`)
+app.listen(config.PORT, () => {
+    logger.info(`Server running on port ${config.PORT}`)
 })
