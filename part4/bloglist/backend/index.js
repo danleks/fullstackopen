efdfd9ce2a9c900cd90087b7aa96ef6db2773580
danleks <danilevich.lks@gmail.com>
2022-07-09 21:36:53 +0200

    create blog schema

--- part4/bloglist/backend/index.js
+++ part4/bloglist/backend/index.js
@@ -1,13 +1,6 @@
 const http = require('http')
 
-const blogSchema = new mongoose.Schema({
-    title: String,
-    author: String,
-    url: String,
-    likes: Number
-})
 
-const Blog = mongoose.model('Blog', blogSchema)
 
 app.listen(config.PORT, () => {
     logger.info(`Server running on port ${config.PORT}`)
